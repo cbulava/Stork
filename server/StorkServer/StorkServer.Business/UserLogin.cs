d31f1acffff0eaf4c5b0cb1d8483bf3a4ddc06ac
Joshua Childress <childrj@purdue.edu>
2016-09-22 19:14:24 -0400

    classes added

--- server/StorkServer/StorkServer.Business/UserLogin.cs
+++ server/StorkServer/StorkServer.Business/UserLogin.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace StorkServer.Business
 {
-    class Class1
+    class UserLogin
     {
+        public struct userEntity
+        {
+            public string email;
+            public string password;
+            public int userId;
+        }
+        //user login
+        //
+
+        public int loginUser(string email, string password)
+        {
+            int status = 1;//0fail/1success
+            //userEntity user = new userEntity();
+            
+             
+
+            //check email/password combination
+
+
+            return status;
+        }
+
+
+
+
     }
 }
