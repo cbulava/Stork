6dab14e85eae73b922a977f1a8759e3c2a19f8cd
Paul Steele <paul_steele@live.com>
2016-09-23 09:21:49 -0400

    added serverresponse class and example implementation of createUser

---
+++ server/StorkServer/StorkServer.Business/ServerResponse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorkServer.Business {
+    public class ServerResponse {
+        private bool success; //the success status of the operation
+        private String message; //the additional message cause by success or failure
+        private Object payload; //if the server also needs to return an object it should be placed in the payload
+
+        public ServerResponse(bool success, String message, Object payload) {
+            this.success = success;
+            this.message = message;
+            this.payload = payload;
+        }
+
+        public bool getSuccess() {
+            return success;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public Object getPayload() {
+            return payload;
+        }
+    }
+}
