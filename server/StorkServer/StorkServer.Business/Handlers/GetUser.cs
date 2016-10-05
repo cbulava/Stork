bdfd389d3528096082e41696340e02ca3437d79e
Rob <rmantzoros@gmail.com>
2016-10-05 11:30:32 -0400

    added widgetTable and table adapter, getUser, and auto increment of Id

--- server/StorkServer/StorkServer.Business/Handlers/GetUser.cs
+++ server/StorkServer/StorkServer.Business/Handlers/GetUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StorkServer.Sql.StorkDatabaseDataSetTableAdapters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace StorkServer.Business.Handlers
 {
-    class GetUser
+    public class GetUser
     {
+        public void getUser(string username)
+        {
+            UserTableTableAdapter newUser = new UserTableTableAdapter();
+
+            newUser.GetDataBy(username);
+        }
     }
 }
---
+++ server/StorkServer/StorkServer.Sql/Databases/WidgetTable.Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[WidgetTable]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [col] INT NOT NULL, 
+    [row] INT NOT NULL, 
+    [sizeX] INT NOT NULL, 
+    [sizeY] INT NOT NULL, 
+    [refreshRate] INT NOT NULL DEFAULT 2, 
+    [stocks] NVARCHAR(50) NULL, 
+    [widgetType] NVARCHAR(50) NOT NULL
+)
