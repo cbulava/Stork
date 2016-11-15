9cb64771f174e7287ad3e3e10ab394fc338695f3
Nathan <scott246@purdue.edu>
2016-11-15 16:20:39 -0500

    autofill component
    added autofill component in shared/components/autofill and
    shared/html/autofill

---
+++ StorkFrontEnd/Stork/src/app/shared/components/autofill.component.ts
@@ -0,0 +1,25 @@
+import { NgGrid, NgGridItem } from 'angular2-grid';
+import { HttpRequestService } from '../../shared/services/http-request.service';
+import { NgGridConfig, NgGridItemConfig, NgGridItemEvent } from "angular2-grid";
+import { WidgetControlService } from '../../shared/services/widget-control.service';
+import { Component, OnInit} from '@angular/core';
+import { CompleterService, CompleterData } from 'ng2-completer';
+import myGlobals = require('../../widgets/widget-stocktable/globals');
+
+@Component({
+    moduleId: module.id,
+    selector: 'autofill',
+})
+export class AutofillComponent implements OnInit {
+    private dataService: CompleterData;
+    private searchData = myGlobals.stocks;
+
+    constructor(private completerService: CompleterService) {
+        this.dataService = completerService.local(this.searchData, 'Symbol,Name', 'Symbol').descriptionField('Name');
+    }
+
+    ngOnInit() { 
+
+    }
+
+}
