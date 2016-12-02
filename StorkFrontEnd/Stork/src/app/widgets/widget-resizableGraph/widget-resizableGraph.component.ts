33e3ed939615e8f36b84128286b08592cc7d5fc3
Nathan <scott246@purdue.edu>
2016-12-02 15:37:36 -0500

    started resizable graph widget
    can choose timeframes, cannot resize :(

---
+++ StorkFrontEnd/Stork/src/app/widgets/widget-resizableGraph/widget-resizableGraph.component.ts
@@ -0,0 +1,115 @@
+
+import { NgGrid, NgGridItem } from 'angular2-grid';
+import { HttpRequestService } from '../../shared/services/http-request.service';
+import { NgGridConfig, NgGridItemConfig, NgGridItemEvent } from "angular2-grid";
+import { WidgetControlService } from '../../shared/services/widget-control.service';
+import { Component, OnInit} from '@angular/core';
+import { Injectable } from '@angular/core';
+import { WidgetSampleComponent } from '../widget-sample/widget-sample.component';
+import { CompleterService, CompleterData } from 'ng2-completer';
+import myGlobals = require('../../widgets/widget-stocktable/globals');
+
+import { ViewChild, ElementRef } from '@angular/core'
+
+interface Box {
+	id: number;
+	config: NgGridItemConfig;
+    data: Array<any>;
+	name: string;
+    type: number;
+    error: string;
+}
+
+@Component({
+    moduleId: module.id,
+    selector: 'widget-resizableGraph',
+    templateUrl: 'widget-resizableGraph.component.html'
+})
+export class ResizableGraphComponent implements OnInit {
+	private boxes: Array<Box> = [];
+    private box: Box;
+	private gridConfig: NgGridConfig;
+    private boxId: number;
+    private basicFields: Array<string> = ["bid", "daysLow", "daysHigh", "yearsLow", "yearsHigh", "ask", "averageDailyVolume", "daysRange"];
+    private showError: boolean = false;
+    private httpData: Array<any>;
+    private dataService: CompleterData;
+    private searchData = myGlobals.stocks;
+
+    
+    constructor(private widgetControl: WidgetControlService, 
+                private completerService: CompleterService,
+                private httpReq: HttpRequestService) {
+        this.dataService = completerService.local(this.searchData, 'Symbol,Name', 'Symbol').descriptionField('Name');
+        this.boxes = this.widgetControl.getBoxes;
+
+        //this is the box you are assigned. This is set in the WidgetHolderComponent when all 
+        //the widgets are assigned to their boxes based on a dashboard(future)
+        this.boxId = this.widgetControl.currentInitBoxId;
+
+        //do you stock retrieval before getting your box to play with!
+        this.widgetControl.getStockData("AAPL", this.boxId, this.basicFields);
+        this.widgetControl.setMinSize(this.boxId, 35, 35);
+        this.widgetControl.updateSize(this.boxId, 35, 35);
+                //get your box!
+        this.box = this.boxes[this.boxId];
+
+        //do your stuff!
+        this.box.name = "Resizable Graph Widget";
+        //this.getSymbolData("AAPL");
+        //the key values are known only to you so that's on you. You can use the data
+        //in the html by using {{ box.data.your_key }}. You can look up pipes to 
+        //fetch the data.
+     }
+
+    refreshGraph(){
+        var container = document.getElementById("data");
+        var content = container.innerHTML;
+        container.innerHTML= content;
+    }
+
+    ngOnInit() { 
+        
+    }
+    public timeframe = String;
+
+    timeframes = [
+         {value:'1d', display:'1 day'},
+         {value:'5d', display:'1 week'},
+         {value:'1m', display:'1 month'}, 
+         {value:'3m', display:'3 months'},
+         {value:'6m', display:'6 months'},
+         {value:'1y', display:'1 year'},
+         {value:'5y', display:'5 years'},
+    ]
+
+
+    @ViewChild('dataContainer') dataContainer: ElementRef;
+//<img src="http://chart.finance.yahoo.com/z?s="+symbol.value+"&t=6m&q=l&l=on&z=s&p=m50,m200"/>
+//this.dataContainer.nativeElement.innerHTML = "<img src=\"http://chart.finance.yahoo.com/z?s=APPL&t=6m&q=l&l=on&z=s&p=m50,m200\"/>";
+    loadData(symbol: string) {
+       
+    }
+
+    getSymbolData(symbol: string){
+        this.httpReq.getStock(symbol, this.basicFields).subscribe(
+            response => {
+                if(response.success){
+                            this.widgetControl.updateSize(this.boxId, 35, 28);
+                            this.dataContainer.nativeElement.innerHTML = "<img src=\"http://chart.finance.yahoo.com/z?s="+symbol+"&t="+this.timeframe+"&q=l&l=on&z=s&p=m50,m200\" width=\"100%\" height=\"100%\"/>";
+                            this.widgetControl.getStockData(symbol, this.boxId, this.basicFields);
+                        }else{
+                            alert("Look up unsuccessful. Please check your symbol and try again.");
+                            //look up failed do stuff here
+                        }
+            },
+            error => {
+				this.showError = true;
+				this.boxes[this.boxId].error = "Error timeout in Server. Server may be slow, or stock data is updating on Yahoo page.";
+			}
+        )
+
+            
+     }
+
+}
