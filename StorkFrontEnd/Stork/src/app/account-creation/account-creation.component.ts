76dddffc508bc1d73c8bb2bd9a4bd1ec2a64c7c5
Christopher M Bulava <cbulava@purdue.edu>
2016-12-08 18:23:02 -0500

    FInal Presentation 2

--- StorkFrontEnd/Stork/src/app/account-creation/account-creation.component.ts
+++ StorkFrontEnd/Stork/src/app/account-creation/account-creation.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpRequestService } from '../shared/services/http-request.service';
 import { Router } from '@angular/router';
+import { ReCaptchaComponent } from 'angular2-recaptcha/lib/captcha.component';
+
 @Component({
     moduleId: module.id,
     selector: 'account-creation',
     templateUrl: 'account-creation.component.html'
 })
+
 export class AccountCreationComponent implements OnInit {
     private httpData: Array<any>;
+    private enableAccountCreation: boolean = false;
+    private buttonColor: string = "gray";
 
     constructor(private httpReq: HttpRequestService, private router: Router) { }
-
+    @ViewChild(ReCaptchaComponent) captcha: ReCaptchaComponent;
     ngOnInit() { 
+        this.enableAccountCreation = false;
+    }
 
+    handleCorrectCaptcha(event: any){
+        this.enableAccountCreation = true;
+        this.buttonColor = "black";
     }
     createAccount(email: string, password: string) {
+        let token = this.captcha.getResponse();
         this.httpReq.createAccount(email, password, password).subscribe(       
                     response => {
                         if(response.success){
